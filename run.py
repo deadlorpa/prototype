4cae150dd2954003e37106d811dc012ea99c528c
deadl <deadl@DESKTOP-4AB4T3P>
2020-06-02 19:38:09 +0700

    Начальная связка opencv и интерфейс на pyQt5

--- run.py
+++ run.py
@@ -1,17 +1,39 @@
 import cv2
 import numpy as np
+import sys
 
-from time import time
+from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QLabel
+from PyQt5 import uic
+from PyQt5.QtWidgets import QDialog
 import motion_detector as detector
+import ui.main as mainWindow
+
+class ExampleApp(QMainWindow, mainWindow.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
 
 
 if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = ExampleApp()
     cap = cv2.VideoCapture(0)
     color = (255, 0, 0)
+    testy = QLabel()
+    testy2 =  QLabel()
+    window.stackedWidget.addWidget(testy)
+    window.stackedWidget.addWidget(testy2)
     success, frame = cap.read()
     while success:
         frame = detector.detect(frame, color)
-        cv2.imshow('origin', frame)
+        window.show()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image = QImage(frame, frame.shape[1],frame.shape[0], frame.shape[1] * 3, QImage.Format_RGB888)
+        testy2.setPixmap(QPixmap(image))
+        testy.setPixmap(QPixmap(image))
         success, frame = cap.read()
-        if cv2.waitKey(30) == 27:
-            break
+        cv2.waitKey(30)
+
+    app.exec_()
